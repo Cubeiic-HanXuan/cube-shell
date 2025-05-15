07e67801fbd628315276e4cc07071b2c9077759a
陈德兵 <768519234@qq.com>
2025-05-15 18:54:52 +0800

    update： 1.优化windows平台的编译脚本，默认使用clang进行编译

--- build-exe.bat
+++ build-exe.bat
@@ -11,7 +11,34 @@ pip install nuitka
 
 REM Step 2: Build the application
 echo 2: Building the application...
-nuitka --windows-console-mode=disable --windows-icon-from-ico=icons/logo.ico --output-dir=deploy --standalone --enable-plugin=pyside6 --follow-imports --include-module=qdarktheme --include-module=deepdiff --include-module=pygments --include-module=paramiko --include-module=pyte --include-module=yaml --include-module=pygments.formatters.html --include-module=pygments.lexers.python --include-package=core,function,style,ui,icons --include-data-dir=conf=conf --include-data-dir=frp=frp cube-shell.py
+nuitka --windows-console-mode=disable --windows-icon-from-ico=icons/logo.ico ^
+  --nofollow-imports ^
+  --remove-output ^
+  --jobs=0 ^
+  --python-flag=no_site ^
+  --lto=yes ^
+  --clang ^
+  --output-dir=deploy ^
+  --standalone ^
+  --enable-plugin=pyside6 ^
+  --follow-imports ^
+  --include-module=qdarktheme ^
+  --include-module=deepdiff ^
+  --include-module=pygments ^
+  --include-module=paramiko ^
+  --include-module=pyte ^
+  --include-module=yaml ^
+  --include-module=pygments.formatters.html ^
+  --include-module=pygments.lexers.python ^
+  --include-package=core,function,style,ui,icons ^
+  --include-data-dir=conf=conf ^
+  --include-data-dir=frp=frp ^
+  --company-name=HanXuan ^
+  --product-name="cubeShell" ^
+  --file-version=1.7.0.0 ^
+  --product-version=1.7.0 ^
+  --file-description="A powerful shell application" ^
+  cube-shell.py
 
 REM Step 3: Create tunnel.json file
 echo 3: Creating tunnel.json file...
